2bf35af667c29b30799fd873efdb1efbd0a06c3c
AaronMillOro <aaron.ibt@gmail.com>
2019-05-13 00:26:29 -0500

    add of routes/views in app.py using the provided HTML/CSS files

--- app.py
+++ app.py
@@ -16,15 +16,30 @@ HOST = '0.0.0.0'
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/entries/')
+@app.route('/', methods=['POST','GET'])
+@app.route('/entries/', methods=['POST','GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/entries/new')
+@app.route('/entries/new/', methods=['POST','GET'])
 def new_entry():
     return render_template('new.html')
+
+
+@app.route('/entries/<id>/', methods=['POST','GET'])
+def entry_details():
+    return render_template('detail.html')
+
+
+@app.route('/entries/<id>/edit/', methods=['POST','GET'])
+def edit_entry():
+    return render_template('edit.html')
+
+
+@app.route('/entries/<id>/delete/', methods=['POST','GET'])
+def delete_entry():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
